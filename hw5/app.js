ef1e5ec7676407c21f8be6299fe8ef0b0f16b6cb
Kostiantyn <k.ichko.ua@gmail.com>
2020-12-24 19:34:25 +0200

    hw5

---
+++ hw5/app.js
@@ -0,0 +1,53 @@
+// --- Task 1
+
+const sumCounter = () => {
+  let sum = 0;
+  return (num) => {
+    return sum += num;
+  };
+};
+
+const counterSem = sumCounter();
+
+// --- Task 2
+
+const UpdatedArrCounter = () => {
+  let arr = [];
+  return (element) => {
+    if (!element) {
+      return arr = [];
+    };
+    arr.push(element);
+    return arr;
+  };
+};
+const getUpdatedArr = UpdatedArrCounter();
+
+// --- Task 3
+
+const getTimeClosure = () =>{
+    let sec = 0;
+    return () =>{
+        if (sec == 0) {
+        	sec = new Date().getTime();
+        	return('Enabled');
+        }
+        let secondSec = new Date().getTime();
+        finalSec = Math.abs(sec - secondSec)
+        sec = secondSec;
+        return Math.ceil(finalSec / 1000);
+    } 
+}
+
+const getTime = getTimeClosure();
+
+// --- Task 4
+
+const timer = (time) => {
+    let time = 0;
+    return () =>{
+
+    }
+}
+
+const counter = timer();
