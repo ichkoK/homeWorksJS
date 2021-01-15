b0f1120bfc1323e8992523273f10a4034351712c
Kostiantyn <k.ichko.ua@gmail.com>
2021-01-15 14:53:08 +0200

    last task

--- hw4/app.js
+++ hw4/app.js
@@ -68,3 +68,33 @@ const myString = new CustomString();
 console.log(myString.reverse("qwerty"));
 console.log(myString.ucFirst("qwerty"));
 console.log(myString.ucWords("qwerty qwerty qwerty"));
+
+// --- Task 3 
+
+class Validator {
+  constructor() {}
+
+  checkIsEmail(str) {
+    return /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(str);
+  }
+
+  checkIsDomain(str) {
+    return /^((?:(?:(?:\w[\.\-\+]?)*)\w)+)((?:(?:(?:\w[\.\-\+]?){0,62})\w)+)\.(\w{2,6})$/.test(str);
+  }
+
+  checkIsDate(str) {
+    return /^([0-9]{2})\.([0-9]{2})\.([0-9]{4})$/.test(str);
+  }
+
+   checkIsPhone(str) {
+    let cleanstr = str.replace(/[^\d]/g,'');
+    return /^380+[0-9]{9}$/g.test(cleanstr);
+  }
+}
+
+var validator = new Validator();
+
+console.log(validator.checkIsEmail('vasya.pupkin@gmail.com')); // true
+console.log(validator.checkIsDomain('google.com')); // true
+console.log(validator.checkIsDate('30.11.2019')); // true
+console.log(validator.checkIsPhone('+38 (066) 337-99-92')); // если код страны Украинский, то возвращаем true иначе false
